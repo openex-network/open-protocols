4b355b2b6203b4104e9ed4e4659ac3eb86c48b7b
agiex <contact@agiex.org>
2024-04-03 16:58:05 +0800

    doc: Airdrop

--- projects/token/OEXToken.sol
+++ projects/token/OEXToken.sol
@@ -20,16 +20,16 @@ contract OEXToken is ERC20 {
     constructor() ERC20("OpenEX Network Token", "OEX") {
         uint256 initialSupply = 10_000_000_000 ether;
 
-        // 10% : Community Airdrop Pool Part 1 - Satoshi App
-        // 10% : Community Airdrop Pool Part 2 - Agiex, OEX, LONG, Mainnet, etc.
+        // 20% : Community Airdrop Pool
+        //  - 10% : Part 1 - Satoshi App
+        //  - 10% : Part 2 - Agiex, OEX, LONG, Mainnet, etc.
         // 30% : Community Staking Rewards Pool
-        // 20% : DAO Pool
+        // 20% : Community DAO Pool
         // 15% : Ecosystem Growth and Marketing Pool
         // 10% : Team Pool
         //  5% : Early Contributors Pool
 
-        _mint(0xbDa427AEBB0dA0Af5214B74bfC87C16Ecfdf5bE0, (initialSupply / 100) * 10);
-        _mint(0x95910805dc57F99934d29cf957a8D3b7fa597d57, (initialSupply / 100) * 10);
+        _mint(0xbDa427AEBB0dA0Af5214B74bfC87C16Ecfdf5bE0, (initialSupply / 100) * 20);
         _mint(0x4C614DF6122D236BBd341BaC86D591a5C3738Ece, (initialSupply / 100) * 30);
         _mint(0x722C064A301736f6437483c940953F47B4be78EE, (initialSupply / 100) * 20);
         _mint(0x909884d5988F92fED88200459977098a3446A2E7, (initialSupply / 100) * 15);
