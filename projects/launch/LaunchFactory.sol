1fab1e4295e3453dd925f37f1b72471df2ffb10f
agiex <contact@agiex.org>
2024-04-14 18:09:33 +0800

    fix: It's recommended to add a balance check to ensure the contract holds a sufficient tokenA balance.

--- projects/launch/LaunchFactory.sol
+++ projects/launch/LaunchFactory.sol
@@ -33,16 +33,19 @@ contract LaunchFactory is Ownable {
             minTokenCreate == 0 || IERC20(passToken).balanceOf(msg.sender) >= minTokenCreate,
             "Not Enough Pass Token"
         );
+        require(_config.tokenA != _config.tokenB, "Token A and Token B cannot be the same");
 
         if (passToken != address(0)) {
             _config.passToken = passToken;
             _config.minPassToken = minTokenExchange;
         }
 
-        uint256 launchToken = _config.maxTotalExchange * 2;
-        _config.tokenA.safeTransferFrom(msg.sender, address(this), launchToken);
+        uint256 launchTokenAmount = _config.maxTotalExchange * 2;
+        require(_config.tokenA.balanceOf(msg.sender) >= launchTokenAmount, "Token A Not Enough");
+
+        _config.tokenA.safeTransferFrom(msg.sender, address(this), launchTokenAmount);
         FairLaunch newLaunch = new FairLaunch(pancakeRouter, _config);
-        _config.tokenA.safeTransfer(address(newLaunch), launchToken);
+        _config.tokenA.safeTransfer(address(newLaunch), launchTokenAmount);
         launches.push(address(newLaunch));
         newLaunch.transferOwnership(msg.sender);
 
